18bb0ae5cbc4d2c8feebdf0f176d60334dbefc96
Anugya Baraik <83193763+Anugya12@users.noreply.github.com>
2022-02-12 08:56:36 +0530

    Update week1_Q5.c

--- week1_Q5.c
+++ week1_Q5.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
     float r,d,c,a;
-    system("cls");
     printf("enter radius of circle=");
     scanf("%f",&r);
     d= 2*r;
@@ -12,5 +10,4 @@ void main()
     printf("diameter of circle=%f",d);
     printf("circumference of circle=%f",c);
  printf("area of circle=%f",a);
- getch();
 }
