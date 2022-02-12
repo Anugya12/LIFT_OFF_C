6562eeac579bbe61cb97ac2eee7e9f5ae1d25ca3
Anugya Baraik <83193763+Anugya12@users.noreply.github.com>
2022-02-12 08:53:57 +0530

    Update week1_Q2.c

--- week1_Q2.c
+++ week1_Q2.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
     int a,b;
-    system("cls");
     printf("Enter 2 numbers:");
     scanf("%d%d",&a,&b);
     if(a>b)
     printf("%d is maximum",a);
     else
     printf("%d is maximum",b);
-getch();
 }
